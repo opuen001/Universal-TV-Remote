97bd1b1cbee08d91af7ce372aafea0275e99d130
ChickensRock4 <opuen001@ucr.edu>
2025-07-24 21:09:42 -0700

    fixed converter.cpp and converted vizio codes

--- convert_codes/converter.cpp
+++ convert_codes/converter.cpp
@@ -7,7 +7,7 @@
 
 int main() {
     std::ifstream inputFile("irdbCodes/vizio.csv");
-    std::ofstream outputFile("../rawTimings/vizio.csv");
+    std::ofstream outputFile("../rawTimings/vizioTimings.csv");
 
     if (!inputFile.is_open() || !outputFile.is_open()) {
         std::cerr << "Error opening files.\n";
@@ -15,34 +15,33 @@ int main() {
     }
 
     std::string line;
+    bool first = true;
     // Read each line from input csv
     while (std::getline(inputFile, line)) {
         std::stringstream ss(line);
         std::string function, protocal;
         int d, s, f;
         char commas;
-        //std::vector<std::string> row;
 
-        // get the function name and information
-        std::getline(ss, function, ',');
-        std::getline(ss, protocal, ',');
-        ss >> d >> commas >> s >> commas >> f;
+        if (first){
+            //ignores the header in each file
+            std::getline(ss,function, '\n');
+            first = false;
+        } else{
+            // get the function name and information
+            std::getline(ss, function, ',');
+            std::getline(ss, protocal, ',');
+            ss >> d >> commas >> s >> commas >> f;
 
-        //create a char* for EncodeIR
-        std::vector<char> prot_char(protocal.begin(), protocal.end());
-        prot_char.push_back('\0');
+            //create a char* for EncodeIR
+            std::vector<char> prot_char(protocal.begin(), protocal.end());
+            prot_char.push_back('\0');
 
-        std::string timings = EncodeIR(prot_char.data(), d, s, f);
+            std::string timings = EncodeIR(prot_char.data(), d, s, f);
 
-        //Output raw timings to new csv
-        outputFile << function << ": " << timings << "\n" << "\n";
-
-        /* Example: write same row to output csv
-        for (size_t i = 0; i < row.size(); ++i) {
-            outputFile << row[i];
-            if (i < row.size() - 1)
-                outputFile << ",";
-        }*/
+            //Output raw timings to new csv
+            outputFile << function << ": " << timings << "\n";
+        }
     }
 
     inputFile.close();
