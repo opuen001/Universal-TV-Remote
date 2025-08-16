e2d9c1208c948c328769d9017c079008a8c37c65
ChickensRock4 <opuen001@ucr.edu>
2025-08-15 21:17:24 -0700

    Added first example

---
+++ examples/vizioPowerButton/powerButton.cpp
@@ -0,0 +1,41 @@
+const int LED_PIN = 16;
+const int POWER_PIN = 5;
+unsigned long timing[68] = {9024, 4512, 564, 564, 564, 564, 564, 1692, 564, 564, 564, 564, 564, 564, 564, 564, 564, 564, 564, 1692, 564, 1692, 564, 564, 564, 1692, 564, 1692, 564, 1692, 564, 1692, 564, 1692, 564, 564, 564, 564, 564, 564, 564, 1692, 564, 564, 564, 564, 564, 564, 564, 564, 564, 1692, 564, 1692, 564, 1692, 564, 564, 564, 1692, 564, 1692, 564, 1692, 564, 1692, 564, 43992};
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(POWER_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  int buttonState = digitalRead(POWER_PIN);
+  if(buttonState == LOW){
+    Serial.println("button pressed");
+    sendIRSignal();
+  }
+  delay(100);
+}
+
+void sendIRSignal() {
+  for (int i = 0; i < 68; i += 2) {
+    // ON with 38kHz modulation
+    enableIROut(timing[i]);
+    //digitalWrite(LED_PIN, HIGH);
+    //delayMicroseconds(timing[i]);
+
+    // OFF
+    digitalWrite(LED_PIN, LOW);
+    delayMicroseconds(timing[i + 1]);
+  }
+}
+
+void enableIROut(unsigned int timeMicros) {
+  unsigned long start = micros();
+  while (micros() - start < timeMicros) {
+    digitalWrite(LED_PIN, HIGH);
+    delayMicroseconds(13);   // ~26µs total ON+OFF gives ~38kHz
+    digitalWrite(LED_PIN, LOW);
+    delayMicroseconds(13);
+  }
+}
