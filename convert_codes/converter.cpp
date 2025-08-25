13790163d58bca4118bf836842010065e8c9c7e8
ChickensRock4 <opuen001@ucr.edu>
2025-08-25 13:23:44 -0700

    Added Sony codes

--- convert_codes/converter.cpp
+++ convert_codes/converter.cpp
@@ -6,8 +6,8 @@
 #include <vector>
 
 int main() {
-    std::ifstream inputFile("irdbCodes/vizio.csv");
-    std::ofstream outputFile("../rawTimings/vizioTimings.csv");
+    std::ifstream inputFile("irdbCodes/sony.csv");
+    std::ofstream outputFile("../rawTimings/sonyTimings.csv");
 
     if (!inputFile.is_open() || !outputFile.is_open()) {
         std::cerr << "Error opening files.\n";
